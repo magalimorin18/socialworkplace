67000628bd8f41cd6e3d08158f5ee4a959f1d44e
Magali Morin <magali.morin@student-cs.fr>
2022-01-20 15:38:34 +0100

    Access Token envoye au back end

--- front-end/tabs/src/Components/Tab.jsx
+++ front-end/tabs/src/Components/Tab.jsx
@@ -9,55 +9,47 @@ import * as microsoftTeams from "@microsoft/teams-js";
 
 export default function Tab() {
   const [groups, setGroups] = useState([]);
+  const [token, setToken] = useState("");
   const [refresh, setRefresh] = useState(false);
 
   const refreshPage = () => {
     setRefresh(!refresh);
   };
 
-  // console.log(microsoftTeams.authentication.getAuthToken());
-  // useEffect(() => {
-  //   console.log(
-  //     `https://login.microsoftonline.com/upslidedev.onmicrosoft.com/oauth2/v2.0/authorize?client_id=02409369-a990-435b-a640-64e953388932&response_type=token&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Fauth-end.html&scope=0b766349-a7a1-4901-ac04-3a6304bf211c/.default&response_mode=fragment`
-  //   );
-  //   console.log(window.location.hash.split("&")[0].substring(14));
-  // }, []); // Tableau de dépendance vide donc appelle que deux fois (quand le compensant est crée et quand il n'est plus affiché)
-
   var authTokenRequest = {
     successCallback: function (result) {
-      console.log("Success: " + result);
+      window.localStorage.setItem("AccessToken", result);
+      setToken(result);
     },
     failureCallback: function (error) {
-      console.log("Failure: " + error);
+      alert("Failure: " + error);
     },
   };
-  //microsoftTeams.authentication.getAuthToken(authTokenRequest);
 
-  // useEffect(() => {
-  //   console.log(`https://localhost:5001/api/Group`);
-  //   fetch("https://localhost:5001/api/Group", { method: "GET" }).then((rep) =>
-  //     rep
-  //       .json()
-  //       .then((groups) => {
-  //         setGroups(groups);
-  //         console.log("je suis dans la fonction App");
-  //         console.log(groups);
-  //       })
-  //       .catch((e) => console.log(e))
-  //   );
-  // }, [refresh]);
+  microsoftTeams.authentication.getAuthToken(authTokenRequest);
+
+  useEffect(() => {
+    if (token) {
+      console.log(`https://localhost:5001/api/Group`);
+      fetch("https://localhost:5001/api/Group", {
+        method: "GET",
+        headers: new Header({ Authorization: `Bearer ${token}` }),
+      }).then((rep) =>
+        rep
+          .json()
+          .then((groups) => {
+            setGroups(groups);
+            console.log("je suis dans la fonction App");
+            console.log(groups);
+          })
+          .catch((e) => console.log(e))
+      );
+    }
+  }, [refresh, token]);
 
   return (
     <div>
       <Header />
-      <button
-        onClick={() => {
-          microsoftTeams.authentication.getAuthToken(authTokenRequest);
-          console.log("onclick");
-        }}
-      >
-        Cliquez
-      </button>
       <NewGroup onAddGroup={refreshPage} items={groups} />
       <Groups
         onDeleteGroup={refreshPage}
