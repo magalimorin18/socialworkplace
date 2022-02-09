4ee836e33ea88fa1b2cefeecbacf28166b5bd392
Hugues Baratgin <hugues.baratgin@gmail.com>
2022-02-09 19:13:20 +0100

    Removing useless import in GroupController.cs

--- backend/Controllers/GroupController.cs
+++ backend/Controllers/GroupController.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Azure.Identity;
 using Microsoft.AspNetCore.Authorization;
