c7cb264ca04cdf9755a3eb4b43e6b7c677423f15
Magali Morin <magali.morin@student-cs.fr>
2022-03-17 17:52:10 +0100

    Deleting useless comments

--- front-end/tabs/src/Components/App.jsx
+++ front-end/tabs/src/Components/App.jsx
@@ -9,40 +9,11 @@ import "./App.css";
 import TabConfig from "./TabConfig";
 
 import Notification from "./UI/Notification";
-// import { useState, useRef } from "react";
-
-// export const NotifContext = React.createContext();
 
 export default function App() {
   const { theme, loading } = useTeamsFx();
 
-  // const [list, setList] = useState();
-  // const listRef = useRef(list);
-  // listRef.current = list;
-
-  // function addNotification(type, text, timeOut = 4000) {
-  //   const newNotif = { id: Math.random(), text, type, fadeIn: true };
-  //   setTimeout(() => {
-  //     setList(
-  //       listRef.current.map((notif) => {
-  //         if (notif.id === newNotif.id) {
-  //           return { ...newNotif, fadeIn: false };
-  //         }
-  //         return notif;
-  //       })
-  //     );
-  //   }, timeOut);
-  //   setList([...list, newNotif]);
-  // }
-
-  // function removeNotification(notif) {
-  //   setList(listRef.current.filter((prevNotif) => prevNotif.id !== notif.id));
-  // }
-
   return (
-    // <NotifContext.Provider
-    //   value={{ add: addNotification, remove: removeNotification }}
-    // >
     <Notification>
       <Provider
         theme={theme || teamsTheme}
@@ -63,9 +34,7 @@ export default function App() {
             </>
           )}
         </Router>
-        {/* <Notification list={list} /> */}
       </Provider>
     </Notification>
-    // </NotifContext.Provider>
   );
 }
